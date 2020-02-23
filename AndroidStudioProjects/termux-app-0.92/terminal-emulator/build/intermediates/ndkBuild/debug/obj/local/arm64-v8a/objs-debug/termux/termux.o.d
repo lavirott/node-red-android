5807ea19ab79d6c445894b06f9e5508ded47ebaf
Balsam CHIHI <balsam-ch@users.noreply.github.com>
2020-02-23 14:07:30 +0100

    updated to latest termux version : stable release for end-user.

---
+++ AndroidStudioProjects/termux-app-0.92/terminal-emulator/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs-debug/termux/termux.o.d
@@ -0,0 +1,2 @@
+C:/Users/Balsam/AndroidStudioProjects/termux-app-0.92/terminal-emulator/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs-debug/termux/termux.o: \
+  C:\Users\Balsam\AndroidStudioProjects\termux-app-0.92\terminal-emulator\src\main\jni\termux.c
